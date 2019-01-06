82869ba602a569278b184892edbfb3473b8d0876
Oscar Paruma <oscar.paruma@gmail.com>
2019-01-05 23:02:13 -0500

    Enums grupo sanguíneo y factor RH

---
+++ app/Enums/GrupoSanguineoEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Artemoc\Enums;
+
+use Artemoc\Enums\Enum;
+
+abstract class GrupoSanguineoEnum extends Enum {
+    const A = 'A';
+    const B = 'B';
+    const AB = 'AB';
+    const O = 'O';
+}
