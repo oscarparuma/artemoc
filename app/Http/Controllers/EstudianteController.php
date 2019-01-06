dc160cb8c0f4ea595fa4749632044450742cf5aa
Oscar Paruma <oscar.paruma@gmail.com>
2019-01-05 23:00:54 -0500

    Controlador Estudiante

---
+++ app/Http/Controllers/EstudianteController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Artemoc\Http\Controllers;
+
+use Artemoc\Estudiante;
+use Illuminate\Http\Request;
+
+class EstudianteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+		$estudiantes = Estudiante::all();
+		return view('estudiantes.index')->with('estudiantes', $estudiantes);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('estudiantes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Artemoc\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Artemoc\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Artemoc\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Artemoc\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Estudiante $estudiante)
+    {
+        //
+    }
+}
