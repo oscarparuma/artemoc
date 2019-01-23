e54a411b7bd1c77636e8257adc7d68de52061d7d
Oscar Paruma <oscar.paruma@gmail.com>
2019-01-23 15:17:50 -0500

    Se modifica el layout principal y la pantalla de inicio de la aplicación.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,16 +2,36 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<title>Emocionarte - Sistema de administraci&oacute;n</title>
-		<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+		
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+		<!-- Custom styles -->
+		<link href="{{ asset('css/artemoc.css') }}" rel="stylesheet" type="text/css" />
+
 		<!-- Jquery -->
-		<script src="{{asset('js/jquery-1.11.3.min.js')}}"></script>
+		<script src="{{ asset('js/jquery-3.1.0.js') }}"></script>
+		<!-- Bootstrap -->
+		<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}""></script>
+		<link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
+
+		<script type="text/javascript" src="{{ asset('moment-2.24.0/moment.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('bootstrap/js/transition.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('bootstrap/js/collapse.js') }}"></script>
+
 		<!-- Datepicker Files -->
-		<link rel="stylesheet" href="{{asset('datePicker/css/bootstrap-datepicker3.css')}}">
-		<link rel="stylesheet" href="{{asset('datePicker/css/bootstrap-datepicker.standalone.css')}}">
-		<script src="{{asset('datePicker/js/bootstrap-datepicker.js')}}"></script>
-		<!-- Languaje -->
-		<script src="{{asset('datePicker/locales/bootstrap-datepicker.es.min.js')}}"></script>
+		<link rel="stylesheet" href="{{ asset('datePicker/css/bootstrap-datepicker3.css') }}">
+		<link rel="stylesheet" href="{{ asset('datePicker/css/bootstrap-datepicker.standalone.css') }}">
+		<script src="{{ asset('datePicker/js/bootstrap-datepicker.js') }}"></script>
+		<!-- Bootstrap Datetimepicker Files -->
+		<script type="text/javascript" src="{{ asset('bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js') }}"></script>
+		<link rel="stylesheet" href="{{ asset('bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css') }}" />
+		<!-- Language -->
+		<script src="{{ asset('datePicker/locales/bootstrap-datepicker.es.min.js') }}"></script>
+		
 	</head>
 	<body>
 		<div class="container">
@@ -25,5 +45,13 @@
 			@endif
 			@yield('content')
 		</div>
+		<script src="{{ asset('js/app.js') }}"></script>
+		<script type="text/javascript">
+			$.ajaxSetup({
+				headers: {
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				}
+			});
+		</script>
 	</body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Artemoc</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -81,16 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Artemoc
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="/estudiantes">Estudiantes</a>
+                    <a href="/servicios">Servicios</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://emocionarte.com.co" target="_blank">Emocionarte</a>
                 </div>
             </div>
         </div>
