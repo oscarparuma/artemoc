c0762da152a75e180bc5b7e52c18c2cbaba0606f
Oscar Paruma <oscar.paruma@gmail.com>
2019-01-23 15:27:45 -0500

    Ajuste para direccionar a la pantalla de inicio de la aplicación.

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Artemoc\Http\Controllers;
+
+class WelcomeController extends Controller {
+
+	public function __construct() {
+		$this->middleware('guest');
+	}
+
+	public function index() {
+		return view('welcome');
+	}
+
+}
