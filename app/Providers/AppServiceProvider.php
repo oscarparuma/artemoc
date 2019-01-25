23e45e5c213d8c253940cf2de88dbaecbed98e38
Oscar Hernán Paruma Pabón <oscar.paruma@gmail.com>
2019-01-24 21:32:25 -0500

    Se registran view composers para los módulos Estudiante, Acudiente y ServicioEstudiante.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Providers;
+namespace Artemoc\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Contracts\View\Factory as ViewFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +13,17 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(ViewFactory $view)
     {
         //
+		Schema::defaultStringLength(191);
+		
+		$view->composer('partials.forms.estudiante',
+            'Artemoc\Http\Views\Composers\EstudianteFormComposer');
+        $view->composer('partials.forms.acudiente',
+            'Artemoc\Http\Views\Composers\AcudienteFormComposer');
+        $view->composer('partials.forms.servicioestudiante',
+			'Artemoc\Http\Views\Composers\ServicioEstudianteFormComposer');
     }
 
     /**
