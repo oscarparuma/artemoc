ccef293ba21d3043272c45a1affd7a9e8a2051b7
Oscar Hernán Paruma Pabón <oscar.paruma@gmail.com>
2019-04-07 17:06:43 -0500

    Se agrega el método toArray.

--- app/Enums/Enum.php
+++ app/Enums/Enum.php
@@ -5,8 +5,12 @@ namespace Artemoc\Enums;
 use ReflectionClass;
 
 abstract class Enum {
-    static function getKeys(){
+    static function getKeys() {
         $class = new ReflectionClass(get_called_class());
         return array_keys($class->getConstants());
+    }
+
+    final public static function toArray() {
+        return (new ReflectionClass(static::class))->getConstants();
     }
 }
